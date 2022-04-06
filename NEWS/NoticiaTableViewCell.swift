405a2a51d56c8d759929469e2bb21d5bdbebfdeb
Abraham Reyes <abrahameyes321@hotmail.com>
2022-04-06 10:14:59 -0700

    Cambios mínimos en la card

---
+++ NEWS/NoticiaTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NoticiaTableViewCell.swift
+//  NEWS
+//
+//  Created by Abraham Sahid Reyes Rodriguez on 06/04/22.
+//
+
+import UIKit
+
+class NoticiaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
